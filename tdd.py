d7b308c609543b38aa100197e4a1da8704029edc
Caleb Lawson <caleb@lawson.rocks>
2017-08-20 13:04:01 -0400

    Loads of changes, implemented threading to speed up requests, added the beginnings of command line arguments

--- tdd.py
+++ tdd.py
@@ -2,6 +2,8 @@
 from collections import defaultdict
 import pytumblr
 import json
+import threading
+import argparse
 
 # Authenticate via OAuth
 client = pytumblr.TumblrRestClient(
@@ -12,8 +14,45 @@ client = pytumblr.TumblrRestClient(
 )
 
 
+class GetPostsThread (threading.Thread):
+    def __init__(self, results, blog_name, offset, limit):
+        threading.Thread.__init__(self)
+        self.results = results
+        self.blog_name = blog_name
+        self.offset = offset
+        self.limit = limit
+
+    def run(self):
+        self.results.append(client.posts(self.blog_name + '.tumblr.com',
+                                         reblog_info=True, notes_info=True, offset=self.offset, limit=self.limit))
+
+
+class GetLikesThread (threading.Thread):
+    def __init__(self, results, blog_name, offset, limit):
+        threading.Thread.__init__(self)
+        self.results = results
+        self.blog_name = blog_name
+        self.offset = offset
+        self.limit = limit
+
+    def run(self):
+        self.results.append(client.blog_likes(
+            self.blog_name + '.tumblr.com', offset=self.offset, limit=self.limit))
+
+
+class ReturnOnlyExistingBlogsThread (threading.Thread):
+    def __init__(self, results, blog_name):
+        threading.Thread.__init__(self)
+        self.results = results
+        self.blog_name = blog_name
+
+    def run(self):
+        self.results.append(client.blog_info(self.blog_name + '.tumblr.com'))
+
+
 def getPosts(blog_name, max_posts):
     results = []
+    threads = []
     offset = 0
     while offset + 20 <= max_posts or max_posts == 0:
         # Tumblr will return a max of 20 posts per API call
@@ -22,18 +61,23 @@ def getPosts(blog_name, max_posts):
         if limit > 20 or limit <= 0:
             limit = 20
 
-        results.append(client.posts(blog_name + '.tumblr.com',
-                                    reblog_info=True, notes_info=True, offset=offset, limit=limit))
+        thread = GetPostsThread(results, blog_name, offset, limit)
+        threads += [thread]
+        thread.start()
 
         # 404 status means blog does not exist
-        if results[0].get('meta') and results[0]['meta']['status'] == 404:
+        if len(results) > 0 and results[0].get('meta') and results[0]['meta']['status'] == 404:
             break
 
-        if max_posts == 0:
+        if len(results) > 0 and max_posts == 0:
             max_posts = results[0]['blog']['total_posts']
 
         offset += 20
 
+    for t in threads:
+        t.join()
+
+    print "Done fetching data."
     return results
 
 
@@ -47,19 +91,23 @@ def getLikes(blog_name, max_posts):
         if limit > 20 or limit <= 0:
             limit = 20
 
-        results.append(client.blog_likes(
-            blog_name + '.tumblr.com', offset=offset, limit=limit))
+        thread = GetLikesThread(results, blog_name, offset, limit)
+        threads += [thread]
+        thread.start()
 
         # 404 status means blog does not exist
-        if results[0].get('meta') and results[0]['meta']['status'] == 404:
+        if len(results) > 0 and results[0].get('meta') and results[0]['meta']['status'] == 404:
             break
-        elif results[0].get('meta') and results[0]['meta']['status'] == 403:
+        elif len(results) > 0 and results[0].get('meta') and results[0]['meta']['status'] == 403:
             break
 
-        if max_posts == 0:
+        if len(results) > 0 and max_posts == 0:
             max_posts = results[0]['liked_count']
 
         offset += 20
+
+    for t in threads:
+        t.join()
 
     return results
 
@@ -81,6 +129,7 @@ def blogInPostsFrequency(blog_name, max_posts):
                 else:
                     reblogged_from_list[post['reblogged_from_name']] = 1
 
+    print "Done calculating frequency."
     return reblogged_from_list
 
 
@@ -140,6 +189,7 @@ def comparePosts(source, destination, max_posts):
     # Actual compare happens here, returns matches out of # of sources
     return [{'matches': len(set(sourceRootReblogURLs).intersection(destinationRootReblogURLs))}, {'num_sources': len(sourceRootReblogURLs)}]
 
+
 def compareLikes(source, destination, max_posts):
     # Get post data
     sourceResults = getLikes(source, max_posts)
@@ -164,7 +214,7 @@ def compareLikes(source, destination, max_posts):
         for like in sourceResult['liked_posts']:
             if like.get('source_url'):
                 sourceLikeURLs.append(like['source_url'])
-            #Else, we're gonna try imperfect matching
+            # Else, we're gonna try imperfect matching
             else:
                 sourceLikeURLs.append(like['reblog_key'])
 
@@ -178,6 +228,7 @@ def compareLikes(source, destination, max_posts):
 
     # Actual compare happens here, returns matches out of # of sources
     return [{'matches': len(set(sourceLikeURLs).intersection(destinationLikeURLs))}, {'num_sources': len(sourceLikeURLs)}]
+
 
 def blogInLikesFrequency(blog_name, max_posts):
     liked_blog_list = defaultdict(int)
@@ -246,19 +297,53 @@ def blogInNotesFrequency(blog_name, note_type, max_posts):
 
     return note_list
 
-# Beware of using this too often, since it performs a lot of API calls
+
 def returnOnlyExistingBlogs(dictionary):
+    threads = []
+    results = []
     clean_dict = defaultdict(int)
 
-    for blog in dictionary:
-        result = client.blog_info(blog + '.tumblr.com')
+    for blog_name in dictionary:
+        thread = ReturnOnlyExistingBlogsThread(results, blog_name)
+        threads += [thread]
+        thread.start()
+
+    for t in threads:
+        t.join()
+
+    for result in results:
         if result.get('meta') and result['meta']['status'] != 200:
             pass
         else:
-            clean_dict[blog] = dictionary[blog]
-
+            blog_name = result['blog']['name']
+            clean_dict[blog_name] = dictionary[blog_name]
+    print "Done filtering blog names."
     return clean_dict
 
-def printInOrder(dictionary, descending):
-    for i in sorted(dictionary, key=dictionary.__getitem__, reverse=descending):
-        print(i + " " + str(dictionary[i]))
+
+def printInOrder(dictionary, descending, limit):
+    print ""
+
+    if limit == 0:
+        limit = len(dictionary)
+
+    for h, i in zip(count(), sorted(dictionary, key=dictionary.__getitem__, reverse=descending)):
+        if h <= limit:
+            print(i + " " + str(dictionary[i]))
+        else:
+            break
+
+    print ""
+
+
+# CONTROL CENTER
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "blog_name", help="the name of the blog you are searching on")
+parser.add_argument(
+    "max_posts", help="the maximum amount of posts to take into consideration", type=int)
+parser.add_argument("top", help="how many results to print", type=int)
+args = parser.parse_args()
+
+printInOrder(returnOnlyExistingBlogs(blogInPostsFrequency(
+    args.blog_name, args.max_posts)), True, args.top)
